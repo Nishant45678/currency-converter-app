6b4f6d0e9c4e838e3dddcfddebad9d86f3888ae7
Nishant Shigwan <nishantshigwan@eng.rizvi.edu.in>
2025-07-01 03:07:58 +0530

    add test cases

--- backend/test/integration/currency.test.js
+++ backend/test/integration/currency.test.js
@@ -3,10 +3,22 @@ import app from "../../app.js"
 import request from "supertest"
 
 
-describe("GET POST REQUEST ON /cuurrency", ()=>{
+describe("GET /currencies", ()=>{
   it("should return all currencies with status 200",async ()=>{
-    const res = await request(app).get("/currencies")
+    const res = await request(app).get("/api/currencies")
     expect(res.status).toBe(200)
-    expect(res.body).have
+  })
+})
+
+describe("POST /convert",()=>{
+  const data = {
+  from:"USD",
+  to:"INR",
+  amount:1
+}
+  it("should return status code 200",async()=>{
+    const res = await request(app).post("/api/convert").send(data)
+    expect(res.status).toBe(200)
+    console.log(res.body)
   })
 })
--- backend/test/integration/historical.test.js
+++ backend/test/integration/historical.test.js
@@ -1,9 +1,8 @@
 import { describe, expect, it } from "vitest";
 import app from "../../app";
 import request from "supertest";
-
 // history
-describe("GET /historical", () => {
+describe("GET /api/historical/trend", () => {
   const data = {
     fromDate: "2024-01-01",
     toDate: new Date(),
@@ -12,18 +11,33 @@ describe("GET /historical", () => {
   };
 
   it("should send 400 if any field is missing", async () => {
-    const {fromDate,...rest} = data
-    const res = await request(app)
-      .post("/historical")
-      .send(rest);
+    const { fromDate, ...rest } = data;
+    const res = await request(app).post("/api/historical/trend").send(rest);
     expect(res.status).toBe(400);
     expect(res.body.message).toBe("All fields are required");
   });
 
   it("should send 200", async () => {
-    const res = await request(app)
-      .post("/historical")
-      .send(data);
+    const res = await request(app).post("/api/historical/trend").send(data);
     expect(res.status).toBe(200);
   });
+});
+
+describe("GET /api/historical/current", () => {
+  const data = {
+    currency: "USD",
+    date: "2025-05-27",
+  };
+  it("should send 400 if any required field is missing", async () => {
+    const res = await request(app).post("/api/historical/current").send({});
+    expect(res.status).toBe(400);
+    expect(res.body.message).haveOwnProperty()
+    expect(res.body.message).toBe("All fields are required");
+  });
+
+  it("should send 200 if all fields are correct",async()=>{
+    const res = await request(app).post("/api/historical/current").send(data)
+    expect(res.status).toBe(200)
+
+  })
 });
